47a52435a3d6e50a60380e92f648e7fda24488b9
Josh Grainge <josh.grainge@hotmail.com>
2023-01-05 13:54:26 -0500

    Added calculator functionality

--- js/index.js
+++ js/index.js
@@ -2,24 +2,32 @@ let equation = "";
 // Only lets one operation to be applied at a time
 let symbolActive = false;
 
+let snarkyMessage = 'Nice try ;)';
+
 const outputText = document.querySelector('#screen-text');
 
 const buttons = document.querySelectorAll('button');
 buttons.forEach(button => button.addEventListener('click', e =>{
-    if(button.textContent === '='){
-        console.log('equals');
-        evaluate();
-    }
-    else if(button.textContent === 'AC'){
-        console.log('cleared');
+    
+    if(equation === snarkyMessage){
         equation = "";
     }
+    
+    if(button.textContent === '='){
+        evaluate();
+    }
+    else if(button.textContent === 'AC' || button.textContent === 'C'){
+        equation = "0";
+        symbolActive = false;
+    }
     else if(isNaN(parseInt(button.textContent))){
-        if(!symbolActive){
-            symbolActive = true;
-            console.log('symbol adds');
-            equation += ` ${button.textContent} `;
+        if(symbolActive){
+            evaluate();
         }
+        else{
+            symbolActive = true;
+        }
+        equation += ` ${button.textContent} `;
     }
     else{
         console.log('number adds');
@@ -42,6 +50,10 @@ function multiply(a, b){
 }
 
 function divide(a, b){
+    if(b == 0){
+        return "Nice try ;)";
+    }
+
     return a / b;
 }
 
@@ -75,5 +87,13 @@ function operator(operator, a, b){
 }
 
 function evaluate(){
+    let components = equation.split(' ');
 
+    // Invalid expression will just return first number
+    if(components.length < 3 || components[2] === ''){
+        equation = components[0];
+        return;
+    }
+    
+    equation = operator(components[1], parseInt(components[0]), parseInt(components[2]));
 }
